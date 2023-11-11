621166f9ea26fe348dd78b8b2b4aca11a75bb9a7
Sayandas35 <150130054+Sayandas35@users.noreply.github.com>
2023-11-11 17:29:58 +0530

    Update

---
+++ PRINT NAME USING DO WHILE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+        int start,n;
+        printf("ENTER THE VALUE OF N :");
+        scanf("%d", &n);
+        do{
+                printf("MY NAME IS SAMIM \n");
+        start++;
+        }
+        while(start<=n);
+
+}
