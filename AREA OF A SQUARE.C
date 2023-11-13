4100349ff6da4e19be9d67227a440c494ea073f4
Sayandas35 <150130054+Sayandas35@users.noreply.github.com>
2023-11-13 08:48:03 +0530

    Update

---
+++ AREA OF A SQUARE.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()  {
+    float side;
+    printf("Enter the side of a square:");
+    scanf("%f",&side);
+    printf("The area of a square is: %f",((float) side * side));
+return 0;
+}
