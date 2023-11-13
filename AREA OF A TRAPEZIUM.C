7a565930fb0e2c095b296a5cb3cd68e31ebec7f1
Sayandas35 <150130054+Sayandas35@users.noreply.github.com>
2023-11-13 08:49:30 +0530

    Update

---
+++ AREA OF A TRAPEZIUM.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()  {
+    float upper, lower, height;
+    printf("Enter the upper side of trapizium:");
+    scanf("%f",&upper);
+    printf("Enter the lower side of trapizium:");
+    scanf("%f",&lower);
+    printf("Enter the height of trapizium:");
+    scanf("%f",&height);
+    printf("The area of a rectangle is: %f",((float) 0.5 * ( upper + lower ) * height));
+return 0;
+}
