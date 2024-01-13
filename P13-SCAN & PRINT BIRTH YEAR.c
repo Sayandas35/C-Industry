488b3e98e9854cafaa74841c2abfca672ad59e7c
Sayan Das <150130054+Sayandas35@users.noreply.github.com>
2024-01-13 10:44:14 +0530

    Update P14-SIMPLE CALCULATOR.C

--- P13-SCAN & PRINT BIRTH YEAR.c
+++ P13-SCAN & PRINT BIRTH YEAR.c
@@ -1,1 +1,16 @@
-u
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter the first number : ");
+    scanf ("%d", &a);
+    printf("Enter the second number : ");
+    scanf ("%d", &b);
+
+    printf("First Number + Second Number : %d\n",a+b);
+    printf("First Number - Second Number : %d\n",a-b);
+    printf("First Number * Second Number : %d\n",a*b);
+    printf("First Number / Second Number : %d\n",a/b);
+
+    return 0;
+}
